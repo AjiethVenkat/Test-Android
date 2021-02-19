cfd146f5281bb4605cf652065563d46de97c0fe0
AjiethVenkat <AjiethVenkat@hotmail.com>
2021-02-19 02:51:37 -0500

    Added Login and Animation Activities

--- app/src/main/java/com/datechnologies/androidtest/animation/AnimationActivity.java
+++ app/src/main/java/com/datechnologies/androidtest/animation/AnimationActivity.java
@@ -1,13 +1,29 @@
 package com.datechnologies.androidtest.animation;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.datechnologies.androidtest.MainActivity;
 import com.datechnologies.androidtest.R;
+import com.skyfishjy.library.RippleBackground;
+
 
 /**
  * Screen that displays the D & A Technologies logo.
@@ -34,6 +50,9 @@ public class AnimationActivity extends AppCompatActivity {
     // Lifecycle Methods
     //==============================================================================================
 
+    ImageView img;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +73,50 @@ public class AnimationActivity extends AppCompatActivity {
         // TODO: The user should be able to touch and drag the D & A Technologies logo around the screen.
 
         // TODO: Add a bonus to make yourself stick out. Music, color, fireworks, explosions!!!
+
+        img = (ImageView) findViewById(R.id.imageView);
+        final RippleBackground rippleBackground = (RippleBackground)findViewById(R.id.content);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.faded);
+
+        rippleBackground.startRippleAnimation();
+
+        img.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    ClipData data = ClipData.newPlainText("", "");
+                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(img);
+
+                    img.startDrag(data, shadowBuilder, img, 0);
+                    img.setVisibility(View.VISIBLE);
+                    rippleBackground.stopRippleAnimation();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        Button fadeButton = (Button) findViewById(R.id.buttonFade);
+        fadeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animfade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ic_fade_effect);
+                img.startAnimation(animfade);
+                rippleBackground.startRippleAnimation();
+                mediaPlayer.start();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/com/datechnologies/androidtest/login/LoginActivity.java
+++ app/src/main/java/com/datechnologies/androidtest/login/LoginActivity.java
@@ -1,13 +1,33 @@
 package com.datechnologies.androidtest.login;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.datechnologies.androidtest.MainActivity;
 import com.datechnologies.androidtest.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A screen that displays a login prompt, allowing the user to login to the D & A Technologies Web Server.
@@ -34,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ActionBar actionBar = getSupportActionBar();
+        final ActionBar actionBar = getSupportActionBar();
 
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -56,6 +76,74 @@ public class LoginActivity extends AppCompatActivity {
         // TODO: email: info@datechnologies.co
         // TODO: password: Test123
         // TODO: so please use those to test the login.
+
+
+        final String  email = "info@datechnologies.co";
+        final String password = "Test123";
+
+        final String postUrl = "http://dev.rapptrlabs.com/Tests/scripts/login.php";
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringRequest jsonObjRequest = new StringRequest(Request.Method.POST,
+                        postUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(final String response) {
+                                Log.i("LoginActivity.this", response);
+
+                                AlertDialog.Builder alertDialog = new AlertDialog.Builder(LoginActivity.this);
+
+                                alertDialog.setTitle("Success");
+
+                                alertDialog.setMessage(response);
+
+                                alertDialog.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                });
+
+                                alertDialog.show();
+                            }
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d("volley", "Error: " + error.getMessage());
+                        error.printStackTrace();
+                    }
+                }) {
+
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/x-www-form-urlencoded; charset=UTF-8";
+                    }
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("email",email);
+                        params.put("password",password);
+                        return params;
+                    }
+
+                };
+
+                requestQueue.add(jsonObjRequest);
+
+            }
+        });
+
+
+
+
     }
 
     @Override
